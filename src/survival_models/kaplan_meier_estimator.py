ae8297507023f19e7055ff892cbf8dfe3b40d67a
gunesevitan <gunesevitan@gmail.com>
2024-12-26 15:37:19 +0300

    kme

--- src/survival_models/kaplan_meier_estimator.py
+++ src/survival_models/kaplan_meier_estimator.py
@@ -61,7 +61,40 @@ if __name__ == '__main__':
     config = yaml.load(open(model_directory / 'config.yaml'), Loader=yaml.FullLoader)
 
     df = pd.read_parquet(settings.DATA / 'datasets' / config['dataset']['name'])
+    df = pd.concat((
+        df,
+        pd.read_csv(settings.DATA / 'folds.csv')
+    ), axis=1, ignore_index=False)
     settings.logger.info(f'Raw Dataset Shape {df.shape}')
+
+    race_groups = df['race_group'].unique()
+
+    for fold in range(1, 6):
+
+        training_mask = df[f'fold{fold}'] == 0
+        validation_mask = df[f'fold{fold}'] == 1
+
+        df.loc[validation_mask, 'kmf_oof_survival_probability'] = 0.
+
+        settings.logger.info(
+            f'''
+            Fold: {fold} 
+            Training: ({training_mask.sum()})
+            Validation: ({validation_mask.sum()})
+            '''
+        )
+
+        kmf = KaplanMeierFitter()
+        kmf.fit(df.loc[training_mask, 'efs_time'], df.loc[training_mask, 'efs'])
+        df.loc[validation_mask, 'kmf_oof_survival_probability'] = kmf.survival_function_at_times(df.loc[validation_mask, 'efs_time']).values
+
+        for race_group in race_groups:
+
+            race_group_mask = df['race_group'] == race_group
+
+            kmf = KaplanMeierFitter()
+            kmf.fit(df.loc[training_mask & race_group_mask, 'efs_time'], df.loc[training_mask & race_group_mask, 'efs'])
+            df.loc[validation_mask & race_group_mask, 'kmf_oof_race_group_survival_probability'] = kmf.survival_function_at_times(df.loc[validation_mask & race_group_mask, 'efs_time']).values
 
     kmf = KaplanMeierFitter()
     kmf.fit(df['efs_time'], df['efs'])
@@ -92,5 +125,8 @@ if __name__ == '__main__':
         group_idx = df_group.index
         df.loc[group_idx, 'kmf_race_group_survival_probability'] = df_group['kmf_race_group_survival_probability']
 
-    df.loc[:, ['kmf_survival_probability', 'kmf_race_group_survival_probability']].to_csv(model_directory / 'survival_probabilities.csv', index=False)
+    df.loc[:, [
+        'kmf_survival_probability', 'kmf_race_group_survival_probability',
+        'kmf_oof_survival_probability', 'kmf_oof_race_group_survival_probability',
+    ]].to_csv(model_directory / 'survival_probabilities.csv', index=False)
     settings.logger.info(f'survival_probabilities.csv is saved to {model_directory}')

51419ebf99a00705a6e028b78c57f1d827fc9758
gunesevitan <gunesevitan@gmail.com>
2025-02-05 10:23:00 +0300

    sksurv linear models

---
+++ src/sksurv_models/linear_model_preprocessing.py
@@ -0,0 +1,167 @@
+from pathlib import Path
+import pickle
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+
+
+def one_hot_encode_categorical_columns(df, categorical_columns, transformer_directory, load_transformers=False):
+
+    """
+    One-hot encode given categorical columns and concatenate them to given dataframe
+
+    Parameters
+    ----------
+    df: pandas.DataFrame
+        Dataframe with categorical columns
+
+    categorical_columns: list
+        List of categorical columns
+
+    transformer_directory: str or pathlib.Path
+        Path of the serialized transformers
+
+    load_transformers: bool
+        Whether to load transformers from the given transformer directory or not
+
+    Returns
+    -------
+    df: pandas.DataFrame
+        Dataframe with encoded categorical columns
+    """
+
+    Path(transformer_directory).mkdir(parents=True, exist_ok=True)
+
+    for column in categorical_columns:
+
+        if load_transformers:
+
+            with open(transformer_directory / f'{column}_encoder.pickle', mode='rb') as f:
+                encoder = pickle.load(f)
+
+            df_column_encoded = pd.DataFrame(
+                encoder.transform(df[column].values.reshape(-1, 1)),
+                columns=[f'{column}_{category}' for category in encoder.categories_[0]]
+            )
+
+            df = pd.concat((df, df_column_encoded), axis=1, ignore_index=False)
+
+        else:
+
+            encoder = OneHotEncoder(
+                categories='auto',
+                drop=None,
+                sparse_output=False,
+                dtype=np.uint8,
+                handle_unknown='error'
+            )
+            encoder.fit(df[column].values.reshape(-1, 1))
+            df_column_encoded = pd.DataFrame(
+                encoder.transform(df[column].values.reshape(-1, 1)),
+                columns=[f'{column}_{category}' for category in encoder.categories_[0]]
+            )
+            df = pd.concat((df, df_column_encoded), axis=1, ignore_index=False)
+
+            with open(transformer_directory / f'{column}_one_hot_encoder.pickle', mode='wb') as f:
+                pickle.dump(encoder, f)
+
+    return df
+
+
+def normalize_continuous_columns(df, continuous_columns, transformer_directory, load_transformers=False):
+
+    """
+    Normalize continuous columns and concatenate them to given dataframe
+
+    Parameters
+    ----------
+    df: pandas.DataFrame
+        Dataframe with continuous columns
+
+    continuous_columns: list
+        List of continuous columns
+
+    transformer_directory: str or pathlib.Path
+        Path of the serialized transformers
+
+    load_transformers: bool
+        Whether to load transformers from the given transformer directory or not
+
+    Returns
+    -------
+    df: pandas.DataFrame
+        Dataframe with encoded continuous columns
+    """
+
+    Path(transformer_directory).mkdir(parents=True, exist_ok=True)
+
+    if load_transformers:
+
+        with open(transformer_directory / 'standard_scaler.pickle', mode='rb') as f:
+            normalizer = pickle.load(f)
+
+        normalized_column_names = [f'{column}_normalized' for column in continuous_columns]
+        df[normalized_column_names] = normalizer.transform(df[continuous_columns].fillna(df[continuous_columns].median()).values)
+
+    else:
+
+        normalizer = StandardScaler()
+        normalizer.fit(df[continuous_columns].values)
+        normalized_column_names = [f'{column}_normalized' for column in continuous_columns]
+        df[normalized_column_names] = normalizer.transform(df[continuous_columns].fillna(df[continuous_columns].median()).values)
+
+        with open(transformer_directory / 'standard_scaler.pickle', mode='wb') as f:
+            pickle.dump(normalizer, f)
+
+    return df
+
+
+def preprocess(
+        df,
+        categorical_columns, continuous_columns,
+        transformer_directory, load_transformers
+):
+
+    """
+    Preprocess given dataframe for survival models
+
+    Parameters
+    ----------
+    df: pandas.DataFrame
+        Dataframe with raw features and targets
+
+    categorical_columns: list
+        List of categorical columns
+
+    continuous_columns: list
+        List of continuous columns
+
+    transformer_directory: str or pathlib.Path
+        Path of the serialized transformers
+
+    load_transformers: bool
+        Whether to load transformers from the given transformer directory or not
+
+    Returns
+    -------
+    df: pandas.DataFrame
+        Preprocessed dataframe
+    """
+
+    df = df.fillna(np.nan)
+
+    df = one_hot_encode_categorical_columns(
+        df=df,
+        categorical_columns=categorical_columns,
+        transformer_directory=transformer_directory,
+        load_transformers=load_transformers
+    )
+
+    df = normalize_continuous_columns(
+        df=df,
+        continuous_columns=continuous_columns,
+        transformer_directory=transformer_directory,
+        load_transformers=load_transformers
+    )
+
+    return df
---
+++ src/sksurv_models/linear_model_trainer.py
@@ -0,0 +1,166 @@
+import sys
+import argparse
+from pathlib import Path
+import pickle
+import yaml
+import json
+import numpy as np
+import pandas as pd
+import sksurv.linear_model
+from sksurv.util import Surv
+
+sys.path.append('..')
+import settings
+import linear_model_preprocessing
+import metrics
+import visualization
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('model_directory', type=str)
+    args = parser.parse_args()
+
+    model_directory = Path(settings.MODELS / args.model_directory)
+    model_directory.mkdir(parents=True, exist_ok=True)
+
+    config = yaml.load(open(model_directory / 'config.yaml'), Loader=yaml.FullLoader)
+
+    df = pd.read_parquet(settings.DATA / 'datasets' / config['dataset']['name'])
+    df = pd.concat((
+        df,
+        pd.read_csv(settings.DATA / 'folds.csv')
+    ), axis=1, ignore_index=False)
+    settings.logger.info(f'Raw Dataset Shape {df.shape}')
+
+    categorical_columns = config['dataset']['categorical_columns']
+    continuous_columns = config['dataset']['continuous_columns']
+
+    df = linear_model_preprocessing.preprocess(
+        df=df,
+        categorical_columns=categorical_columns,
+        continuous_columns=continuous_columns,
+        transformer_directory=settings.DATA / 'linear_model_transformers',
+        load_transformers=False
+    )
+
+    folds = config['training']['folds']
+    features = config['training']['features']
+    time_column = config['training']['time']
+    event_column = config['training']['event']
+
+    settings.logger.info(
+        f'''
+        Running sksurv linear model trainer {config['model_class']}
+        Dataset Shape: {df.shape}
+        Folds: {folds}
+        Time: {time_column}
+        Event: {event_column}
+        Features: {json.dumps(features, indent=2)}
+        '''
+    )
+
+    df_coefficients = pd.DataFrame(
+        data=np.zeros((len(features), len(folds))),
+        index=features,
+        columns=folds
+    )
+    scores = []
+
+    for fold in folds:
+
+        training_mask = df[f'fold{fold}'] == 0
+        validation_mask = df[f'fold{fold}'] == 1
+
+        df.loc[validation_mask, 'prediction'] = 0.
+
+        settings.logger.info(
+            f'''
+            Fold: {fold} 
+            Training: ({np.sum(training_mask)})
+            Validation: ({np.sum(validation_mask)})
+            '''
+        )
+
+        model = getattr(sksurv.linear_model, config['model_class'])(**config['model_parameters'])
+        model.fit(
+            X=df.loc[training_mask, features],
+            y=Surv.from_dataframe(event='efs', time='efs_time', data=df.loc[training_mask]),
+        )
+        model_file_name = f'model_fold_{fold}.pickle'
+        with open(model_directory / model_file_name, mode='wb') as f:
+            pickle.dump(model, f)
+        settings.logger.info(f'{model_file_name} is saved to {model_directory}')
+
+        df_coefficients.loc[:, fold] = np.sum(model.coef_, axis=-1) if len(model.coef_.shape) > 1 else model.coef_
+
+        df.loc[validation_mask, 'prediction'] = model.predict(df.loc[validation_mask, features])
+        validation_scores = metrics.score(
+            df=df.loc[validation_mask],
+            group_column='race_group',
+            time_column='efs_time',
+            event_column='efs',
+            prediction_column='prediction'
+        )
+        settings.logger.info(f'Fold: {fold} - Validation Scores: {json.dumps(validation_scores, indent=2)}')
+        scores.append(validation_scores)
+
+    scores = pd.DataFrame(scores)
+    settings.logger.info(
+        f'''
+        Mean Validation Scores
+        ----------------------
+        {json.dumps(scores.mean(axis=0).to_dict(), indent=2)}
+
+        Standard Deviations
+        -------------------
+        ±{json.dumps(scores.std(axis=0).to_dict(), indent=2)}
+
+        Fold Scores
+        -----------
+        Micro Concordance Index: {scores['micro_concordance_index'].values.tolist()}
+        Macro Concordance Index: {scores['macro_concordance_index'].values.tolist()}
+        Std Concordance Index: {scores['std_concordance_index'].values.tolist()}
+        Stratified Concordance Index: {scores['stratified_concordance_index'].values.tolist()}
+        '''
+    )
+
+    oof_mask = df['prediction'].notna()
+    oof_scores = metrics.score(
+        df=df.loc[oof_mask],
+        group_column='race_group',
+        time_column='efs_time',
+        event_column='efs',
+        prediction_column='prediction'
+    )
+    settings.logger.info(f'OOF Scores: {json.dumps(oof_scores, indent=2)}')
+
+    scores = pd.concat((
+        scores,
+        pd.DataFrame([oof_scores])
+    )).reset_index(drop=True)
+    scores['fold'] = folds + ['OOF']
+    scores = scores[scores.columns.tolist()[::-1]]
+    scores.to_csv(model_directory / 'scores.csv', index=False)
+    settings.logger.info(f'scores.csv is saved to {model_directory}')
+
+    visualization.visualize_scores(
+        scores=scores,
+        title=f'lifelines Regression Model Scores of {len(folds)} Fold(s)',
+        path=model_directory / 'scores.png'
+    )
+    settings.logger.info(f'Saved scores.png to {model_directory}')
+
+    df_coefficients['mean'] = df_coefficients[config['training']['folds']].mean(axis=1)
+    df_coefficients['std'] = df_coefficients[config['training']['folds']].std(axis=1).fillna(0)
+    df_coefficients.sort_values(by='mean', ascending=False, inplace=True)
+    visualization.visualize_feature_importance(
+        df_feature_importance=df_coefficients,
+        title=f'lifelines Regression Model Coefficients',
+        path=model_directory / f'coefficients.png'
+    )
+    settings.logger.info(f'Saved coefficients.png to {model_directory}')
+
+    df.loc[:, 'prediction'].to_csv(model_directory / 'oof_predictions.csv', index=False)
+    settings.logger.info(f'oof_predictions.csv is saved to {model_directory}')

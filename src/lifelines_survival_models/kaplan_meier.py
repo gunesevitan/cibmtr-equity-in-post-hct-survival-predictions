cef18ccc38906998a5f92408d4ed6fe63e5e031a
gunesevitan <gunesevitan@gmail.com>
2025-02-07 15:32:41 +0300

    kaplan meier

--- src/lifelines_survival_models/kaplan_meier.py
+++ src/lifelines_survival_models/kaplan_meier.py
@@ -40,31 +40,31 @@ if __name__ == '__main__':
             '''
         )
 
-        kmf = KaplanMeierFitter()
-        kmf.fit(df.loc[training_mask, 'efs_time'], df.loc[training_mask, 'efs'])
-        df.loc[validation_mask, 'oof_survival_probability'] = kmf.survival_function_at_times(df.loc[validation_mask, 'efs_time']).values
+        model = KaplanMeierFitter()
+        model.fit(df.loc[training_mask, 'efs_time'], df.loc[training_mask, 'efs'])
+        df.loc[validation_mask, 'oof_survival_probability'] = model.survival_function_at_times(df.loc[validation_mask, 'efs_time']).values
 
         for race_group in race_groups:
             race_group_mask = df['race_group'] == race_group
-            kmf = KaplanMeierFitter()
-            kmf.fit(df.loc[training_mask & race_group_mask, 'efs_time'], df.loc[training_mask & race_group_mask, 'efs'])
-            df.loc[validation_mask & race_group_mask, 'oof_race_group_survival_probability'] = kmf.survival_function_at_times(df.loc[validation_mask & race_group_mask, 'efs_time']).values
+            model = KaplanMeierFitter()
+            model.fit(df.loc[training_mask & race_group_mask, 'efs_time'], df.loc[training_mask & race_group_mask, 'efs'])
+            df.loc[validation_mask & race_group_mask, 'oof_race_group_survival_probability'] = model.survival_function_at_times(df.loc[validation_mask & race_group_mask, 'efs_time']).values
 
-    kmf = KaplanMeierFitter()
-    kmf.fit(df['efs_time'], df['efs'])
-    df['survival_probability'] = kmf.survival_function_at_times(df['efs_time']).values
+    model = KaplanMeierFitter()
+    model.fit(df['efs_time'], df['efs'])
+    df['survival_probability'] = model.survival_function_at_times(df['efs_time']).values
 
-    visualization.visualize_survival_probabilities(
-        kmf=kmf,
+    visualization.visualize_survival_function(
+        model=model,
         title='Kaplan-Meier Fitter Survival Probabilities',
         path=model_directory / 'survival_probabilities.png'
     )
     settings.logger.info(f'survival_probabilities.png is saved to {model_directory}')
 
     for race_group, df_group in df.groupby('race_group'):
-        kmf = KaplanMeierFitter()
-        kmf.fit(df_group['efs_time'], df_group['efs'])
-        df_group['race_group_survival_probability'] = kmf.survival_function_at_times(df_group['efs_time']).values
+        model = KaplanMeierFitter()
+        model.fit(df_group['efs_time'], df_group['efs'])
+        df_group['race_group_survival_probability'] = model.survival_function_at_times(df_group['efs_time']).values
         group_idx = df_group.index
         df.loc[group_idx, 'race_group_survival_probability'] = df_group['race_group_survival_probability']
 

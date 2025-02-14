8ec7a198806092e0c8b07935a7daabf05c153be8
gunesevitan <gunesevitan@gmail.com>
2025-02-14 18:38:29 +0300

    pp

--- src/gbdt_models/preprocessing.py
+++ src/gbdt_models/preprocessing.py
@@ -51,13 +51,35 @@ def create_targets(df):
     """
 
     df['log_efs_time'] = np.log(df['efs_time'])
+    df['inverted_efs_time'] = df['efs_time'].values
+    df.loc[df['efs'] == 0, 'inverted_efs_time'] *= -1
 
-    df_kaplan_meier = pd.read_csv(settings.MODELS / 'kaplan_meier' / 'targets.csv')
+    df_kaplan_meier = pd.read_csv(settings.MODELS / 'kaplan_meier' / 'targets.csv').rename(columns={
+        'survival_probability': 'km_survival_probability'
+    })
+    df_nelson_aalen = pd.read_csv(settings.MODELS / 'nelson_aalen' / 'targets.csv').rename(columns={
+        'cumulative_hazard': 'na_cumulative_hazard'
+    })
     df = pd.concat((
         df,
-        df_kaplan_meier
+        df_kaplan_meier,
+        df_nelson_aalen
     ), axis=1, ignore_index=False)
-    df['log_survival_probability'] = np.log(df['survival_probability'])
+
+    df['log_km_survival_probability'] = np.log(df['km_survival_probability'])
+    df['log_km_survival_probability'] -= df['log_km_survival_probability'].min()
+    df['log_km_survival_probability'] /= df['log_km_survival_probability'].max()
+    df['exp_na_cumulative_hazard'] = -np.exp(df['na_cumulative_hazard'])
+    df['exp_na_cumulative_hazard'] -= df['exp_na_cumulative_hazard'].min()
+    df['exp_na_cumulative_hazard'] /= df['exp_na_cumulative_hazard'].max()
+
+    return df
+
+
+def create_sample_weights(df):
+
+    df['weight'] = 1.
+    df.loc[df['efs'] == 0, 'weight'] = 0.9
 
     return df
 
@@ -75,7 +97,7 @@ def preprocess(df, categorical_columns, categorical_dtype):
     categorical_columns: list
         Array of categorical column names
 
-    dtype: str
+    categorical_dtype: str
         Type of the categorical column
 
     Returns
@@ -86,6 +108,7 @@ def preprocess(df, categorical_columns, categorical_dtype):
 
     df = df.fillna(np.nan)
     df = create_targets(df=df)
+    df = create_sample_weights(df=df)
     df = encode_categorical_columns(df=df, categorical_columns=categorical_columns, dtype=categorical_dtype)
 
     return df

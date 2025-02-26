e588fa65b618b77b5a52922d848a6d00728f3b84
gunesevitan <gunesevitan@gmail.com>
2025-02-26 10:19:06 +0300

    lgb trainer

--- src/gbdt_models/lightgbm_trainer.py
+++ src/gbdt_models/lightgbm_trainer.py
@@ -53,12 +53,9 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('model_directory', type=str)
-    parser.add_argument('mode', type=str)
     args = parser.parse_args()
 
     model_directory = Path(settings.MODELS / args.model_directory)
-    model_directory.mkdir(parents=True, exist_ok=True)
-
     config = yaml.load(open(model_directory / 'config.yaml'), Loader=yaml.FullLoader)
 
     df = pd.read_parquet(settings.DATA / 'datasets' / config['dataset']['name'])
@@ -68,11 +65,12 @@ if __name__ == '__main__':
     ), axis=1, ignore_index=False)
     settings.logger.info(f'Raw Dataset Shape {df.shape}')
 
-    categorical_columns = config['dataset']['categorical_columns']
-
     df = preprocessing.preprocess(
         df=df,
-        categorical_columns=categorical_columns, categorical_dtype='category'
+        categorical_columns=config['dataset']['categorical_columns'], categorical_dtype='category',
+        kaplan_meier_targets_path=config['dataset']['kaplan_meier_targets_path'],
+        efs_predictions_path=config['dataset']['efs_predictions_path'],
+        efs_weight=config['training']['efs_weight']
     )
 
     task = config['training']['task']
@@ -84,8 +82,8 @@ if __name__ == '__main__':
 
     settings.logger.info(
         f'''
-        Running LightGBM trainer in {args.mode} mode
-        Processed Dataset Shape: {df.shape}
+        Running LightGBM trainer for {task} task
+        Dataset Shape: {df.shape}
         Folds: {folds}
         Features: {json.dumps(features, indent=2)}
         Categorical Features: {json.dumps(categorical_features, indent=2)}
@@ -93,139 +91,114 @@ if __name__ == '__main__':
         '''
     )
 
-    if args.mode == 'training':
+    df_feature_importance_gain = pd.DataFrame(
+        data=np.zeros((len(features), len(folds))),
+        index=features,
+        columns=folds
+    )
+    df_feature_importance_split = pd.DataFrame(
+        data=np.zeros((len(features), len(folds))),
+        index=features,
+        columns=folds
+    )
+    scores = []
+    if task == 'classification':
+        curves = []
+    else:
+        curves = None
 
-        df_feature_importance_gain = pd.DataFrame(
-            data=np.zeros((len(features), len(folds))),
-            index=features,
-            columns=folds
-        )
-        df_feature_importance_split = pd.DataFrame(
-            data=np.zeros((len(features), len(folds))),
-            index=features,
-            columns=folds
-        )
-        scores = []
-        seed_scores = {}
+    for fold in folds:
 
-        for fold in folds:
+        training_mask = df[f'fold{fold}'] == 0
+        validation_mask = df[f'fold{fold}'] == 1
 
-            training_mask = df[f'fold{fold}'] == 0
-            validation_mask = df[f'fold{fold}'] == 1
-            df.loc[validation_mask, 'prediction'] = 0.
+        if config['training']['two_stage']:
+            training_mask = training_mask & (df['efs'] == 1)
 
-            settings.logger.info(
-                f'''
-                Fold: {fold} 
-                Training: ({training_mask.sum()}) - Target Mean: {df.loc[training_mask, target].mean():.4f}
-                Validation: ({validation_mask.sum()}) - Target Mean: {df.loc[validation_mask, target].mean():.4f}
-                '''
-            )
+        df.loc[validation_mask, 'prediction'] = 0.
 
-            for seed in seeds:
-
-                training_dataset = lgb.Dataset(
-                    df.loc[training_mask, features],
-                    label=df.loc[training_mask, target],
-                    categorical_feature=categorical_features,
-                    weight=df.loc[training_mask, 'weight'] if config['training']['sample_weight'] else None
-                )
-                validation_dataset = lgb.Dataset(
-                    df.loc[validation_mask, features],
-                    label=df.loc[validation_mask, target],
-                    categorical_feature=categorical_features,
-                    weight=df.loc[validation_mask, 'weight'] if config['training']['sample_weight'] else None
-                )
-
-                config['model_parameters']['seed'] = seed
-                config['model_parameters']['feature_fraction_seed'] = seed
-                config['model_parameters']['bagging_seed'] = seed
-                config['model_parameters']['drop_seed'] = seed
-                config['model_parameters']['data_random_seed'] = seed
-
-                model = lgb.train(
-                    params=config['model_parameters'],
-                    train_set=training_dataset,
-                    valid_sets=[training_dataset, validation_dataset],
-                    num_boost_round=config['fit_parameters']['boosting_rounds'],
-                    callbacks=[
-                        lgb.log_evaluation(config['fit_parameters']['log_evaluation'])
-                    ]
-                )
-                model_file_name = f'model_fold_{fold}_seed_{seed}.lgb'
-                model.save_model(model_directory / model_file_name, num_iteration=None, start_iteration=0)
-                settings.logger.info(f'{model_file_name} is saved to {model_directory}')
-
-                df_feature_importance_gain[fold] += pd.Series((model.feature_importance(importance_type='gain') / len(seeds)), index=features)
-                df_feature_importance_split[fold] += pd.Series((model.feature_importance(importance_type='split') / len(seeds)), index=features)
-
-                validation_predictions = model.predict(df.loc[validation_mask, features], num_iteration=config['fit_parameters']['boosting_rounds'])
-                if config['training']['rank_transform']:
-                    validation_predictions = pd.Series(validation_predictions).rank(pct=True).values
-                df.loc[validation_mask, 'prediction'] += validation_predictions / len(seeds)
-
-            if task == 'ranking':
-                validation_scores = metrics.ranking_score(
-                    df=df.loc[validation_mask],
-                    group_column='race_group',
-                    time_column='efs_time',
-                    event_column='efs',
-                    prediction_column='prediction'
-                )
-            elif task == 'classification':
-                validation_scores = metrics.classification_score(
-                    df=df.loc[validation_mask],
-                    group_column='race_group',
-                    event_column='efs',
-                    prediction_column='prediction',
-                    threshold=0.5
-                )
-            elif task == 'regression':
-                validation_scores = metrics.regression_score(
-                    df=df.loc[validation_mask],
-                    group_column='race_group',
-                    time_column=target,
-                    prediction_column='prediction'
-                )
-            else:
-                raise ValueError(f'Invalid task type {task}')
-
-            settings.logger.info(f'Fold: {fold} - Validation Scores: {json.dumps(validation_scores, indent=2)}')
-            scores.append(validation_scores)
-
-        scores = pd.DataFrame(scores)
         settings.logger.info(
             f'''
-            Mean Validation Scores
-            ----------------------
-            {json.dumps(scores.mean(axis=0).to_dict(), indent=2)}
-            
-            Standard Deviations
-            -------------------
-            ±{json.dumps(scores.std(axis=0).to_dict(), indent=2)}
+            Fold: {fold} 
+            Training: ({np.sum(training_mask)}) - Target Mean: {df.loc[training_mask, target].mean():.4f}
+            Validation: ({np.sum(validation_mask)}) - Target Mean: {df.loc[validation_mask, target].mean():.4f}
             '''
         )
 
-        oof_mask = df['prediction'].notna()
+        for seed in seeds:
+
+            training_dataset = lgb.Dataset(
+                df.loc[training_mask, features],
+                label=df.loc[training_mask, target],
+                categorical_feature=categorical_features,
+                weight=df.loc[training_mask, 'weight'] if config['training']['sample_weight'] else None
+            )
+            validation_dataset = lgb.Dataset(
+                df.loc[validation_mask, features],
+                label=df.loc[validation_mask, target],
+                categorical_feature=categorical_features,
+                weight=df.loc[validation_mask, 'weight'] if config['training']['sample_weight'] else None
+            )
+
+            config['model_parameters']['seed'] = seed
+            config['model_parameters']['feature_fraction_seed'] = seed
+            config['model_parameters']['bagging_seed'] = seed
+            config['model_parameters']['drop_seed'] = seed
+            config['model_parameters']['data_random_seed'] = seed
+
+            model = lgb.train(
+                params=config['model_parameters'],
+                train_set=training_dataset,
+                valid_sets=[validation_dataset],
+                num_boost_round=config['fit_parameters']['boosting_rounds'],
+                callbacks=[
+                    lgb.log_evaluation(config['fit_parameters']['log_evaluation'])
+                ]
+            )
+            model_file_name = f'model_fold_{fold}_seed_{seed}.lgb'
+            model.save_model(model_directory / model_file_name, num_iteration=None, start_iteration=0)
+            settings.logger.info(f'{model_file_name} is saved to {model_directory}')
+
+            df_feature_importance_gain[fold] += pd.Series((model.feature_importance(importance_type='gain') / len(seeds)), index=features)
+            df_feature_importance_split[fold] += pd.Series((model.feature_importance(importance_type='split') / len(seeds)), index=features)
+
+            validation_predictions = model.predict(df.loc[validation_mask, features], num_iteration=config['fit_parameters']['boosting_rounds'])
+
+            if config['training']['two_stage']:
+                if config['training']['target'] == 'log_efs_time':
+                    validation_predictions = df.loc[validation_mask, 'efs_prediction'] / np.exp(validation_predictions)
+                elif config['training']['target'] == 'log_km_survival_probability':
+                    validation_predictions = df.loc[validation_mask, 'efs_prediction'] * np.exp(validation_predictions)
+
+            if config['training']['rank_transform']:
+                validation_predictions = pd.Series(validation_predictions).rank(pct=True).values
+
+            df.loc[validation_mask, 'prediction'] += validation_predictions / len(seeds)
+
         if task == 'ranking':
-            oof_scores = metrics.ranking_score(
-                df=df.loc[oof_mask],
+            validation_scores = metrics.ranking_score(
+                df=df.loc[validation_mask],
                 group_column='race_group',
                 time_column='efs_time',
                 event_column='efs',
                 prediction_column='prediction'
             )
         elif task == 'classification':
-            oof_scores = metrics.classification_score(
-                df=df.loc[oof_mask],
+            validation_scores = metrics.classification_score(
+                df=df.loc[validation_mask],
                 group_column='race_group',
                 event_column='efs',
-                prediction_column='prediction',
-                threshold=0.5
+                prediction_column='prediction'
             )
+            validation_curves = metrics.classification_curves(
+                df=df.loc[validation_mask],
+                event_column='efs',
+                prediction_column='prediction',
+            )
+            curves.append(validation_curves)
         elif task == 'regression':
-            oof_scores = metrics.regression_score(
-                df=df.loc[oof_mask],
+            validation_scores = metrics.regression_score(
+                df=df.loc[validation_mask],
                 group_column='race_group',
                 time_column=target,
                 prediction_column='prediction'
@@ -233,34 +206,92 @@ if __name__ == '__main__':
         else:
             raise ValueError(f'Invalid task type {task}')
 
-        settings.logger.info(f'OOF Scores: {json.dumps(oof_scores, indent=2)}')
+        settings.logger.info(f'Fold: {fold} - Validation Scores: {json.dumps(validation_scores, indent=2)}')
+        scores.append(validation_scores)
 
-        scores = pd.concat((
-            scores,
-            pd.DataFrame([oof_scores])
-        )).reset_index(drop=True)
-        scores['fold'] = folds + ['OOF']
-        scores = scores[scores.columns.tolist()[::-1]]
-        scores.to_csv(model_directory / 'scores.csv', index=False)
-        settings.logger.info(f'scores.csv is saved to {model_directory}')
+    scores = pd.DataFrame(scores)
+    settings.logger.info(
+        f'''
+        Mean Validation Scores
+        ----------------------
+        {json.dumps(scores.mean(axis=0).to_dict(), indent=2)}
+        
+        Standard Deviations
+        -------------------
+        ±{json.dumps(scores.std(axis=0).to_dict(), indent=2)}
+        '''
+    )
 
-        visualization.visualize_scores(
-            scores=scores,
-            title=f'LightGBM Model Scores of {len(folds)} Fold(s)',
-            path=model_directory / 'scores.png'
+    oof_mask = df['prediction'].notna()
+    if task == 'ranking':
+        oof_scores = metrics.ranking_score(
+            df=df.loc[oof_mask],
+            group_column='race_group',
+            time_column='efs_time',
+            event_column='efs',
+            prediction_column='prediction'
         )
-        settings.logger.info(f'Saved scores.png to {model_directory}')
+    elif task == 'classification':
+        oof_scores = metrics.classification_score(
+            df=df.loc[oof_mask],
+            group_column='race_group',
+            event_column='efs',
+            prediction_column='prediction'
+        )
+    elif task == 'regression':
+        oof_scores = metrics.regression_score(
+            df=df.loc[oof_mask],
+            group_column='race_group',
+            time_column=target,
+            prediction_column='prediction'
+        )
+    else:
+        raise ValueError(f'Invalid task type {task}')
 
-        for importance_type, df_feature_importance in zip(['gain', 'split'], [df_feature_importance_gain, df_feature_importance_split]):
-            df_feature_importance['mean'] = df_feature_importance[config['training']['folds']].mean(axis=1)
-            df_feature_importance['std'] = df_feature_importance[config['training']['folds']].std(axis=1).fillna(0)
-            df_feature_importance.sort_values(by='mean', ascending=False, inplace=True)
-            visualization.visualize_feature_importance(
-                df_feature_importance=df_feature_importance,
-                title=f'LightGBM Feature Importance ({importance_type.capitalize()})',
-                path=model_directory / f'feature_importance_{importance_type}.png'
-            )
-            settings.logger.info(f'Saved feature_importance_{importance_type}.png to {model_directory}')
+    settings.logger.info(f'OOF Scores: {json.dumps(oof_scores, indent=2)}')
 
-        df.loc[:, 'prediction'].to_csv(model_directory / 'oof_predictions.csv', index=False)
-        settings.logger.info(f'Saved oof_predictions.csv to {model_directory}')
+    scores = pd.concat((
+        scores,
+        pd.DataFrame([oof_scores])
+    )).reset_index(drop=True)
+    scores['fold'] = folds + ['OOF']
+    scores = scores[scores.columns.tolist()[::-1]]
+    scores.to_csv(model_directory / 'scores.csv', index=False)
+    settings.logger.info(f'scores.csv is saved to {model_directory}')
+
+    visualization.visualize_scores(
+        scores=scores,
+        title=f'LightGBM Model Scores of {len(folds)} Fold(s)',
+        path=model_directory / 'scores.png'
+    )
+    settings.logger.info(f'Saved scores.png to {model_directory}')
+
+    if task == 'classification':
+
+        visualization.visualize_roc_curves(
+            roc_curves=[curve['roc'] for curve in curves],
+            title=f'LightGBM Model Validation ROC Curves',
+            path=model_directory / 'roc_curves.png'
+        )
+        settings.logger.info(f'Saved roc_curves.png to {model_directory}')
+
+        visualization.visualize_pr_curves(
+            pr_curves=[curve['pr'] for curve in curves],
+            title=f'LightGBM Model Validation PR Curves',
+            path=model_directory / 'pr_curves.png'
+        )
+        settings.logger.info(f'Saved pr_curves.png to {model_directory}')
+
+    for importance_type, df_feature_importance in zip(['gain', 'split'], [df_feature_importance_gain, df_feature_importance_split]):
+        df_feature_importance['mean'] = df_feature_importance[config['training']['folds']].mean(axis=1)
+        df_feature_importance['std'] = df_feature_importance[config['training']['folds']].std(axis=1).fillna(0)
+        df_feature_importance.sort_values(by='mean', ascending=False, inplace=True)
+        visualization.visualize_feature_importance(
+            df_feature_importance=df_feature_importance,
+            title=f'LightGBM Feature Importance ({importance_type.capitalize()})',
+            path=model_directory / f'feature_importance_{importance_type}.png'
+        )
+        settings.logger.info(f'Saved feature_importance_{importance_type}.png to {model_directory}')
+
+    df.loc[:, 'prediction'].to_csv(model_directory / 'oof_predictions.csv', index=False)
+    settings.logger.info(f'Saved oof_predictions.csv to {model_directory}')
